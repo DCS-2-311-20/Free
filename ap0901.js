81433fd0b53aced6afd69feed5e4a651cb4e48ae
Takashi MINOHARA <minohara@cs.takushoku-u.ac.jp>
2022-11-29 13:15:35 +0900

    initial code

---
+++ ap0901.js
@@ -0,0 +1,56 @@
+//
+// 応用プログラミング 第9,10回 自由課題 (ap0901.js)
+// G18400-2021 拓殖太郎
+//
+"use strict"; // 厳格モード
+
+// ライブラリをモジュールとして読み込む
+import * as THREE from "three";
+import { GUI } from "gui";
+
+// ３Ｄページ作成関数の定義
+function init() {
+  // 制御変数の定義
+  const param = {
+    axes: true, // 座標軸
+  };
+
+  // GUIコントローラの設定
+  const gui = new GUI();
+  gui.add(param, "axes").name("座標軸");
+
+  // シーン作成
+  const scene = new THREE.Scene();
+
+  // 座標軸の設定
+  const axes = new THREE.AxesHelper(18);
+  scene.add(axes);
+
+  // カメラの作成
+  const camera = new THREE.PerspectiveCamera(
+    50, window.innerWidth/window.innerHeight, 0.1, 1000);
+  camera.position.set(1,2,3);
+  camera.lookAt(0,0,0);
+
+  // レンダラの設定
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, innerHeight);
+    document.getElementById("output").appendChild(renderer.domElement);
+
+  // 描画処理
+
+  // 描画関数
+  function render() {
+    // 座標軸の表示
+    axes.visible = param.axes;
+    // 描画
+    renderer.render(scene, camera);
+    // 次のフレームでの描画要請
+    requestAnimationFrame(render);
+  }
+
+  // 描画開始
+  render();
+}
+
+init();
